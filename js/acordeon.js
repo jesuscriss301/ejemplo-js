934868ea164827a6c3c45e72bc3b5271671e05ca
jesusmanuelcp <jesusmanuelcp@ufps.edu.co>
2021-11-14 21:48:49 -0500

    incluir boton pagina web e imagen QR por ycada universidad

--- js/acordeon.js
+++ js/acordeon.js
@@ -11,13 +11,25 @@ function cargarUniversidades()
     "\n </button>" +
     "\n </h2>" +
     "\n <div id='flush-collapseOne' class='accordion-collapse collapse' aria-labelledby='flush-headingOne' data-bs-parent='#accordionFlushExample'>" +
-    "\n <div class='accordion-body'>" +"\n &&"+ "\n </div>" +
+    "\n <div class='accordion-body'>" +
+    "<div class='container'>"+
+    "<div class='row align-items-start'>"+
+    "<div class='col-9'>"+
+    "<a href='&&'> &&</a>"+
+
+    "</div>"+
+    "<div class='col-3'>"+
+    "<img src='https://api.qrserver.com/v1/create-qr-code/?size=150x50&data=&&' />"+
+    "</div>"+
+    "</div>"+
+    "</div>" +
+    "\n </div>" +
     "\n </div>" +
     "\n </div>"
 	fetch(url, {method:"GET", headers: { origin:"dominio.com" }}).then(response=>response.json()).then(response=>{
 
 		for (var i = 0; i < response.length; i++) {
-			m+=fila.replace("@@",response[i].name).replace("&&",response[i].web_pages[0]).replaceAll("flush-collapseOne","flush-collapse"+i).replaceAll("flush-headingOne","flush-heading"+i);
+			m+=fila.replace("@@",response[i].name).replaceAll("&&",response[i].web_pages[0]).replaceAll("flush-collapseOne","flush-collapse"+i).replaceAll("flush-headingOne","flush-heading"+i);
 		}
 
 		m=table+m+"\n</div>";
